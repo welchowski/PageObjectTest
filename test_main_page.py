3ea68e84177d9ac362b22b4b8365e65c5931e3df
svarchevskyiyan@gmail.com <84012931+welchowski@users.noreply.github.com>
2021-08-15 00:28:23 +0300

    update initialixation LoginPage class

--- test_main_page.py
+++ test_main_page.py
@@ -9,8 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()
 
     page.should_be_login_link()     # выполняем метод страницы — переходим на страницу логина
-    page_1=LoginPage(browser, MainPageLocators.LINK)
-    page_1.should_be_login_page()
+    login_page=LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
     

9da471ba614049bd0562dfb0566df0ecf3994277
svarchevskyiyan@gmail.com <84012931+welchowski@users.noreply.github.com>
2021-08-14 23:48:18 +0300

    added new test 'should_be_login_page'

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,16 @@
 from .pages.main_page import MainPage
-
+from .pages.locators import MainPageLocators
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    
+    page = MainPage(browser, MainPageLocators.LINK)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()
+
     page.should_be_login_link()     # выполняем метод страницы — переходим на страницу логина
+    page_1=LoginPage(browser, MainPageLocators.LINK)
+    page_1.should_be_login_page()
 
 
     

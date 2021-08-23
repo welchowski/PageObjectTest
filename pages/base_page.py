6b381dae623c7bf6c60d2a6c6a94d0a3ec0c07d9
svarchevskyiyan@gmail.com <84012931+welchowski@users.noreply.github.com>
2021-08-23 20:47:13 +0300

    added is_not_element_present()

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoAlertPresentException # в начале файла
 import math
 import time
@@ -34,5 +37,14 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        
+        return False
+        
     
         

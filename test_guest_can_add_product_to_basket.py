4da2b0848a38a84bef7d1121c5ab7c590eb82925
svarchevskyiyan@gmail.com <84012931+welchowski@users.noreply.github.com>
2021-08-23 20:18:56 +0300

    added parametrize(fixed)

--- test_guest_can_add_product_to_basket.py
+++ test_guest_can_add_product_to_basket.py
@@ -8,9 +8,9 @@ from .pages.base_page import BasePage
 import time
 from selenium import webdriver
 
-#@pytest.mark.parametrize('code', [0,1,2,3,4,5,6, pytest.param(7, marks=pytest.mark.xfail),8,9])
-def test_guest_can_add_product_to_basket(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
+@pytest.mark.parametrize('code', [0,1,2,3,4,5,6, pytest.param(7, marks=pytest.mark.xfail),8,9])
+def test_guest_can_add_product_to_basket(browser, code):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{code}"
     print(link)
     page = ProductPage(browser, link)
     
